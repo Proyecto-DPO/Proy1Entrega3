43f2abf3d6fc9e541884da85131ed37c121671a0
nruip2 <n.ruizp2@uniandes.edu.co>
2023-03-18 15:28:13 -0500

    ..

--- Proyecto1Entrega3/src/Clases/Aplicacion.java
+++ Proyecto1Entrega3/src/Clases/Aplicacion.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Aplicacion {
-    private Hotel hotel = new Hotel(); //hola1
+    private Hotel hotel = new Hotel();
     public static void main(String args[]) throws IOException{
         Aplicacion aplicacion = new Aplicacion();
         aplicacion.ejecutarAplicacion();
